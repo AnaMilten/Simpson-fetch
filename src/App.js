8433a7818fc7b921022457365d9135d290ed99ae
AnaMilten <anaismiltenberger@gmail.com>
2018-10-08 21:18:08 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -18,18 +18,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // on peut mettre notre sampleEmployee par défaut
-      // afin d'avoir toujours un employé d'affiché
       item: sampleQuote,
     };
   }
   
   getQuote() {
-    // Récupération de l'employé via fetch
     fetch("https://thesimpsonsquoteapi.glitch.me/quotes?count=5")
       .then(response  =>  response.json())
       .then(data  => {
-        // Une fois les données récupérées, on va mettre à jour notre state avec les nouvelles données
         this.setState({
           item: data[0],
         });
